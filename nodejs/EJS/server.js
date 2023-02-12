9e2a85988cef79b6808c0137bc4dd70965146d75
Gustavo Matoso <117685566+GustavoMatoso@users.noreply.github.com>
2023-02-12 17:28:00 -0300

    add organization

--- nodejs/EJS/server.js
+++ nodejs/EJS/server.js
@@ -5,12 +5,12 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("pages/index");
 })
 
 app.get("/sobre", (req, res) => {
-    res.render("about");
+    res.render("pages/about");
 })
 
 app.listen(8080);
-console.log("RODANDO !");
+console.log("SERVER RODANDO !");
