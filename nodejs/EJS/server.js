5a0b8eb1cad4f6959d645515633ffe49599c86de
Gustavo Matoso <117685566+GustavoMatoso@users.noreply.github.com>
2023-02-14 10:48:06 -0300

    add passagem objeto js para pag. html

--- nodejs/EJS/server.js
+++ nodejs/EJS/server.js
@@ -5,12 +5,54 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("pages/index");
-})
+
+    const items = [
+        {
+            title: "Carro",
+            cor: "vermelho"
+        },
+        {
+            title: "Carro",
+            cor: "azul"
+        },
+        {
+            title: "Carro",
+            cor: "amarelo"
+        },
+        {
+            title: "Carro",
+            cor: "verde"
+        }
+    ];
+
+    const items2 = [
+        {
+            title: "Moto",
+            cor: "roxa"
+        },
+        {
+            title: "Moto",
+            cor: "dourada"
+        },
+        {
+            title: "Moto",
+            cor: "preta"
+        },
+        {
+            title: "Moto",
+            cor: "cinza"
+        }
+    ];
+
+    res.render("pages/index", {
+        quatroRodas: items, duasRodas: items2,
+    });
+});
 
 app.get("/sobre", (req, res) => {
-    res.render("pages/about");
-})
+    const bemvindo = "Seja muito bem vindo!"
+    res.render("pages/about", {bemvindo: bemvindo,});
+});
 
 app.listen(8080);
 console.log("SERVER RODANDO !");
